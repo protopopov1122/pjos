bf669e8f0099a18cedf759480d275cf25ba60e47
J. Protopopovs <jprotopopov1122@gmail.com>
2022-06-02 22:41:09 +0200

    Improve tests

--- tests/all.sh
+++ tests/all.sh
@@ -10,3 +10,5 @@ done
 python ./generate_test.py "$SAT_SOLVER" 10000 30 php 1 8 1 8
 python ./generate_test.py "$SAT_SOLVER" 1000 30 count 7 9 3 5
 python ./generate_test.py "$SAT_SOLVER" 50000 30 random 3 10 50
+python ./generate_test.py "$SAT_SOLVER" 1000 30 random 4 10 100
+python ./generate_test.py "$SAT_SOLVER" 1000 30 random 4 12 130
--- tests/generate_test.py
+++ tests/generate_test.py
@@ -46,20 +46,6 @@ if __name__ == '__main__':
             sys.stdout.write('+' if res else '-')
             sys.stdout.flush()
         print()
-
-        # num_of_variables = int(sys.argv[2])
-        # num_of_clauses = int(sys.argv[3])
-        # max_clause_width = int(sys.argv[4])
-        # num_of_rounds = int(sys.argv[5])
-        # time_budget = int(sys.argv[6])
-        # sys.stdout.write(f'Random test(Variables: {num_of_variables}; Clauses: {num_of_clauses}; ' +
-        #                    f'Max. clause width: {max_clause_width}; {num_of_rounds} rounds; Time budget: {time_budget}): ')
-        # sys.stdout.flush()
-        # for _ in range(num_of_rounds):
-        #     res = run_solver_test(sys.argv[1], generate_formula(num_of_variables, num_of_clauses, max_clause_width), time_budget)
-        #     sys.stdout.write('+' if res else '-')
-        #     sys.stdout.flush()
-        # print()
     except:
         traceback.print_exc()
         exit(-1)
