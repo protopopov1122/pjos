dbcd6d80f13ec313aa1e164f1e3571896dffbb2c
J. Protopopovs <jprotopopov1122@gmail.com>
2022-06-06 21:58:09 +0200

    Fix test runner script

--- tests/all.sh
+++ tests/all.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 set -xe
+SCRIPT_DIR="$(dirname $0)"
 SAT_SOLVER="$1"
 
-for sample in $(find samples -type f -print);
+for sample in $(find $SCRIPT_DIR/samples -type f -print);
 do
-    python ./sample_test.py "$SAT_SOLVER" 180 "$sample"
+    python "$SCRIPT_DIR/sample_test.py" "$SAT_SOLVER" 180 "$sample"
 done
-python ./generate_test.py "$SAT_SOLVER" 10000 30 php 1 8 1 8
-python ./generate_test.py "$SAT_SOLVER" 1000 30 count 7 9 3 5
-python ./generate_test.py "$SAT_SOLVER" 50000 30 random 3 10 50
-python ./generate_test.py "$SAT_SOLVER" 1000 30 random 4 10 100
-python ./generate_test.py "$SAT_SOLVER" 1000 30 random 4 12 130
+python "$SCRIPT_DIR/generate_test.py" "$SAT_SOLVER" 10000 30 php 1 8 1 8
+python "$SCRIPT_DIR/generate_test.py" "$SAT_SOLVER" 1000 30 count 7 9 3 5
+python "$SCRIPT_DIR/generate_test.py" "$SAT_SOLVER" 50000 30 random 3 10 50
+python "$SCRIPT_DIR/generate_test.py" "$SAT_SOLVER" 1000 30 random 4 10 100
+python "$SCRIPT_DIR/generate_test.py" "$SAT_SOLVER" 1000 30 random 4 12 130
